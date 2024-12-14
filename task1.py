7a32291660b7ab2418da3eb22ccaca4dd551e50c
Molisa3 <liza310721@gmail.com>
2024-12-14 14:37:54 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -8,7 +8,7 @@ print(2*(2+2))
 print((2*2+2) == (2*(2+2)))
 # 4th program
 print(float('123.456'))
-print(float('123.456')* 10)
-print(float('123.456') // 10)
-print(float('123.456') // 10 / 3)
-print(int(float('123.456') // 10 / 3))
+print((float('123.456')* 10) % 10)
+print(int((float('123.456')* 10) % 10))
+
+
