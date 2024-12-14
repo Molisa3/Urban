32af90deb74d86477edb93ecb3eab4c9eb95f35d
Molisa3 <liza310721@gmail.com>
2024-12-14 13:44:33 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,14 @@
+from operator import truediv
+
+name = 'Елизавета'
+print(name)
+age = 25
+print(age)
+new_age = age + 1
+print(new_age)
+age = new_age
+print(age)
+is_student = bool('True')
+print(is_student)
+
+
